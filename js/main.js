ecb60b28446bfe7d885a1cbf12e1d30a524a4ff6
jjaen47 <whimsically47@gmail.com>
2021-08-19 13:08:56 +0800

    added animations and click
    added popup menu for the mobie ver and some animation for the buttons in web version

--- js/main.js
+++ js/main.js
@@ -17,3 +17,35 @@
  * 1) Make the responsive menu work!
  * 2) Implement a modal window - "Sign up now" CTA when someone clicks the 'Sign Up' button
  */ 
+
+ document.querySelector(".rover-cta").addEventListener("click", function(){
+    document.querySelector(".rover-cta").style.backgroundColor = "red"; 
+    document.querySelector(".rover-cta").innerText = "Thanks!";
+})
+
+var modal = document.getElementById("myMenu");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".rover-header-right-mobile");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
